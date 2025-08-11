2eb3ff7bb2a3e552283ecfe03d18157b4fb37e6e
Andrew Kaiser <kaisea.rpi@gmail.com>
2025-08-11 10:19:47 -0400

    impl(web): better human readable numbers, fix arrow focus control

--- web/src/routes/browse/components/Footer.svelte
+++ web/src/routes/browse/components/Footer.svelte
@@ -4,6 +4,24 @@
 
   let {controller, height = $bindable()}: {controller: BrowseController, height: number} = $props()
   const runes = controller.runes
+
+
+  function human_readable_number(n: number) {
+    const number_string = n.toString()
+    const number_groups: string[] = []
+    let current_group: string = ''
+    for (const digit of Array.from(number_string).reverse()) {
+      current_group += digit
+      if (current_group.length >= 3) {
+        number_groups.push(current_group)
+        current_group = ''
+      }
+    }
+    if (current_group) {
+      number_groups.push(current_group)
+    }
+    return Array.from(number_groups.join(',')).reverse().join('')
+  }
 </script>
 
 <style>
@@ -66,7 +84,7 @@
     </div>
 
     <span class="">
-      Total results {runes.media_list.total}
+      Total results {human_readable_number(runes.media_list.total)}
     </span>
   </div>
 </footer>
