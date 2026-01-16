e399edbf6d78416939ddd2d3100597c26175f865
Andrew Kaiser <kaisea.rpi@gmail.com>
2026-01-16 15:02:06 -0500

    fix(cli): logger.error supports Error::cause

--- packages/core/src/lib/logger.ts
+++ packages/core/src/lib/logger.ts
@@ -33,6 +33,20 @@ class Logger extends log.Logger {
       super(name, log_level, {handlers: [CONSOLE_HANDLER]})
     }
   }
+
+  // note that this method override exists because @std/log does not natively print error causes
+  public override error(message: unknown) {
+    if (message instanceof Error) {
+      let error = message
+      let error_stack_chain = [error.stack]
+      while (error.cause) {
+        error = error.cause as Error
+        error_stack_chain.push(`Caused by ` + error.stack)
+      }
+      super.error(error_stack_chain.join('\n'))
+    }
+    else super.error(message)
+  }
 }
 
 export { Logger, LOG_LEVELS }
