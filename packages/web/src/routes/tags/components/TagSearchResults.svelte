0d26872c2dba14b51862ed870a4dcceb3f7d0911
Andrew Kaiser <kaisea.rpi@gmail.com>
2026-03-19 00:38:18 -0400

    clean up view

--- packages/web/src/routes/tags/components/TagSearchResults.svelte
+++ packages/web/src/routes/tags/components/TagSearchResults.svelte
@@ -3,6 +3,8 @@
   import Datetime from '$lib/components/Datetime.svelte'
   import Scroller from '$lib/components/Scroller.svelte'
   import type { TagsController } from '../controller.ts'
+    import { goto } from '$app/navigation';
+    import Numeric from '$lib/components/Numeric.svelte';
 
   let { controller }: { controller: TagsController } = $props()
   const { queryparams } = controller.runes
@@ -23,6 +25,10 @@
     if (queryparams.current.sort_by !== column) return ''
     return queryparams.current.order === 'asc' ? ' \u25B2' : ' \u25BC'
   }
+
+  function pretty_numbers(val: number) {
+    return `${val}`
+  }
 </script>
 
 {#if queryparams.results.length === 0 && !queryparams.loading}
@@ -39,6 +45,7 @@
     <table class="w-full text-sm text-left text-slate-300">
       <thead class="text-sm text-slate-800 border-b border-slate-500 sticky top-0 bg-gray-600">
         <tr>
+          <th class="py-2 px-3">Color</th>
           <th class="py-2 px-3">Tag</th>
           <th class="py-2 px-3">Group</th>
           <th class="py-2 px-3 text-right">
@@ -65,21 +72,28 @@
           </th>
         </tr>
       </thead>
-      <tbody>
+      <tbody class="text-slate-300">
         {#each queryparams.results as tag (tag.id)}
-          <tr class="border-b border-slate-700 hover:bg-slate-700">
+          <tr
+            class="border-b border-slate-700 hover:bg-slate-700 cursor-pointer transition-colors"
+            onclick={() => goto(`/tags/${tag.slug}`)}
+          >
             <td class="py-2 px-3">
+              <span class=" px-2 rounded-sm" style="background-color: {tag.color}">{@html '&nbsp;'}</span>
+            </td>
+            <td class="py-2 px-3 flex font-bold">
               <a
                 href="/tags/{tag.slug}"
-                class="hover:underline">
-                <Tag {tag} show_group={false} hide_count />
+                class="hover:underline w-full">
+                {tag.name}
+                <!-- <Tag {tag} show_group={false} hide_count /> -->
               </a>
             </td>
-            <td class="py-2 px-3 text-slate-400">{tag.group || '(default)'}</td>
-            <td class="py-2 px-3 text-right">{tag.media_reference_count}</td>
-            <td class="py-2 px-3 text-right">{tag.unread_media_reference_count}</td>
-            <td class="py-2 px-3 text-right">{tag.alias_count || ''}</td>
-            <td class="py-2 px-3 text-right">{tag.parent_count || ''}</td>
+            <td class="py-2 px-3 font-bold">{tag.group || '(default)'}</td>
+            <td class="py-2 px-3 text-right text-slate-400"><Numeric number={tag.media_reference_count}/></td>
+            <td class="py-2 px-3 text-right text-slate-400"><Numeric number={tag.unread_media_reference_count} hide_zero/></td>
+            <td class="py-2 px-3 text-right text-slate-400"><Numeric number={tag.alias_count} hide_zero/></td>
+            <td class="py-2 px-3 text-right text-slate-400"><Numeric number={tag.parent_count} hide_zero/></td>
             <td class="py-2 px-3"><Datetime value={tag.created_at} class="text-slate-400" /></td>
             <td class="py-2 px-3"><Datetime value={tag.updated_at} class="text-slate-400" /></td>
           </tr>
